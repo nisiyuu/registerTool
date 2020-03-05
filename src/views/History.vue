9c49afe3e6a8e9f936734aa594199aaee65c82a7
yuki nishimine <nisiyuu@yukinoMacBook-Pro.local>
2020-03-05 18:01:42 +0900

    add deletefunc and validation

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="font">
     <v-timeline
+    dense
+    class="ma-0"
     >
       <v-timeline-item
         v-for="(item,i) in itemlist"
@@ -9,14 +11,16 @@
         color="black"
       >
         <span slot="opposite">{{item.time.toDate()}}</span>
-        <v-card class="elevation-2">
+        <v-card class="elevation-2" :color="item.color">
           <v-row align="center">
-          <v-col cols=12 lg=6 md=6 sm=12 align="center"><img :src="item.samplemain" class="img_size"></v-col>
-          <v-col cols=12 lg=6 md=6 sm=12 class="ml-0" align="center">
-          <v-card outlined class="mt-3"><div>商品タイプ:{{item.type}}</div></v-card>
-          <v-card outlined class="mt-3"><div>商品ID:{{item.groupID}}</div></v-card>
-          <v-card outlined class="mt-3"><div>商品名:{{item.name}}</div></v-card>
-          <v-card outlined class="mt-3"><div>商品概要:{{item.description}}</div></v-card>
+          <v-col cols=12 lg=6 md=6 sm=12 align="center" class="mt-3"><img :src="item.samplemain" class="img_size"></v-col>
+          <v-col cols=12 lg=5 md=5 sm=12 class="ml-0">
+          <v-card class="mt-3"><div>商品タイプ:{{item.type}}</div></v-card>
+          <v-card class="mt-3"><div>商品ID:{{item.groupID}}</div></v-card>
+          <v-card class="mt-3"><div>商品名:{{item.name}}</div></v-card>
+          <v-card class="mt-3"><div>商品概要:{{item.description}}</div></v-card>
+          <v-card class="mt-3"><div>登録日時:{{item.time.toDate()}}</div></v-card>
+          <v-col align="end"><v-btn class="mt-3" color="black" @click="deleteConfirm(item.groupID)" style="color:white" x-small>削除</v-btn></v-col>
           </v-col>
           </v-row>
         </v-card>
@@ -32,8 +36,18 @@ export default {
       itemlist:null,
     }
   },
-    async mounted(){
+  async mounted(){
     this.itemlist = await this.$store.dispatch('getItems');
+  },
+  methods:{
+    deleteConfirm(ida){
+        if (confirm('削除してよろしいですか？')) {
+         this.deleteData(ida)
+        }
+      },
+      deleteData(id){
+        this.$store.dispatch('deleteData',id);
+      }
   },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
-      <v-col>
-        <v-btn @click="login" class="login_position" align="center" color="black"><span style="color:white">ログイン</span></v-btn>
-      </v-col>
+<div>
+  <v-row justify="center">
+  <v-col cols=12 lg=12 md=12 sm=12
+  >
+  <img :src="img.icon" v-for="(img,i) in imgs" :key="i" class="login_position_image">
+  </v-col>
+  </v-row>
+  <div class="login_position_text">商品管理ツールのデモです</div>
+    <v-btn @click="login" class="login_position" align="center" color="black"><span style="color:white">管理画面へ</span></v-btn>
+</div>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      imgs:[
+      {icon:'./img/original.png'},
+      ]
+    }
+  },
   methods: {
     login(){
       this.$store.dispatch('login');
@@ -18,9 +32,45 @@ export default {
 <style scoped>
 .login_position{
   position: absolute;
+  top: 55%;
+  left: 50%;
+  -webkit-transform : translate(-50%,-50%);
+  transform : translate(-50%,-50%);
+}
+
+.login_position_text{
+  font-size: calc(0.75rem + ((1.9vw - 4.8px) * 0.9677));
+  font-weight: 900;
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  -webkit-transform : translate(-50%,-50%);
+  transform : translate(-50%,-50%);
+}
+
+.login_position_image{
+
+  width: 50%;
+  height:auto;
+  position: absolute;
   top: 50%;
   left: 50%;
   -webkit-transform : translate(-50%,-50%);
   transform : translate(-50%,-50%);
 }
+
+
+@media screen and (max-width:700px) {
+.login_position_image{
+
+  width: 90%;
+  height:auto;
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  -webkit-transform : translate(-50%,-50%);
+  transform : translate(-50%,-50%);
+}
+}
+
 </style>
